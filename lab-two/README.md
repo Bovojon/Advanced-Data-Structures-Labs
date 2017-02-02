bb54bc8e979ec4cb7bcb9e2b23ffb8240a21b386
bovojon <bovojon@gmail.com>
2017-02-02 14:20:21 -0500

    Complete Lab 2 - Splay Trees

---
+++ lab-two/README.md
@@ -0,0 +1,4 @@
+# To run the test:
+* Open the terminal
+* `cd` into jon-lab2
+* run  `./testrun.sh` to run the tests for the splay tree and to view the use cases.
--- lab-two/test-splay.py
+++ lab-two/test-splay.py
@@ -3,10 +3,6 @@ Jon Abdulloev
 
 splay-test.py
 
-Notes:
-- 
-- 
-- 
 """
 
 
@@ -27,7 +23,7 @@ class TestSplayTree(unittest.TestCase):
         for i in range(0,len(self.arr)):
             self.splaytree[self.arr[i]] = self.array[i]
             
-            
+# LOOKUP TESTS            
     # Get the ROOT key
     def test_getRootKey(self):
         self.assertEqual(self.splaytree.root().key(), 13)
@@ -38,7 +34,6 @@ class TestSplayTree(unittest.TestCase):
         #self.assertEqual(self.beforeRoot.key(), 11)
         self.assertEqual(self.splaytree.before(self.splaytree.root()).key(), 11)
 
-        
     # Get key after the ROOT
     def test_getAfterRoot(self):
         self.afterRoot = self.splaytree.after(self.splaytree.root())
@@ -48,22 +43,69 @@ class TestSplayTree(unittest.TestCase):
     def test_getLastKey(self):
         self.assertEqual(self.splaytree.last().key(), 58)
         
-        
     # Lookup 11 and test if it is the root after the lookup
     def test_lookupEleven(self):
         self.assertTrue(self.splaytree.find_position(11))
         self.assertEqual(self.splaytree.root().key(), 11)
         
+        
+# INSERT TESTS 
     # Insert into splay tree (25, 100) and test if root key is 25 after insertion
-    def test_insert(self):
+    def test_insert_twen_five(self):
         self.splaytree[25] = 100
         self.assertEqual(self.splaytree.root().key(), 25)
         
-    # Delete key 26 - root key must be 24 after deletion
-    def test_delItem(self):
+    # Insert into splay tree (9, 101) and test if root key is 9 after insertion
+    def test_insert_nine(self):
+        self.splaytree[9] = 101
+        self.assertEqual(self.splaytree.root().key(), 9)
+    
+    # Insert into splay tree (56, 102) and test if root key is 25 after insertion
+    def test_insert_fifty_six(self):
+        self.splaytree[56] = 102
+        self.assertEqual(self.splaytree.root().key(), 56)
+        
+    # Insert into splay tree (12, 103) and test if root key is 25 after insertion
+    def test_insert_twelve(self):
+        self.splaytree[12] = 103
+        self.assertEqual(self.splaytree.root().key(), 12)
+        
+# DELETE TESTS
+    # Delete internal node with key 26 - root key must be 24 after deletion
+    def test_delItem_internal(self):
+        # Check if new tree with root 13 if built
+        self.assertEqual(self.splaytree.root().key(), 13)
         del self.splaytree[26]
         self.assertEqual(self.splaytree.root().key(), 24)
         
+    # DELETE root node - check if root has key 11 after deletion
+    def test_delete_root(self):
+        rootItem = self.splaytree.root()
+        self.splaytree.delete(rootItem)
+        self.assertEqual(self.splaytree.root().key(), 11)
+    
+    # Delete the last and check if new root has key 48 after deletion
+    def test_delete_last(self):
+        del self.splaytree[58]
+        self.assertEqual(self.splaytree.root().key(), 48)
+        
+# Other Trivial Tests from BST:
+    # Find the node with maximum key
+    def test_find_max(self):
+        self.assertEqual(self.splaytree.find_max(), (58, 30))
+    
+    # Find the node with minimum key
+    def test_find_min(self):
+        self.assertEqual(self.splaytree.find_min(), (11, 57))
+        
+    # Find the node with greatest key strictly less than k = key
+    def test_find_lt(self):
+        self.assertTrue(self.splaytree.find_lt(40), 30)
+                      
+    # Find the node with least key strictly greater than k.
+    def test_find_gt(self):
+        self.assertEqual(self.splaytree.find_gt(48), (58,30))
+
 
  
         
